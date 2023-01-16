448a19df593b40522213204a896f848655c064d6
OlegMarkoF <o.mark@mail.ru>
2023-01-16 20:03:27 +0200

    реализован попап image

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ const popupImageElement = document.querySelector('.popup_image');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const popupCloseAddButtonElement = popupAddElement.querySelector('.popup__close_add');
 const popupCloseEditButtonElement = popupEditElement.querySelector('.popup__close_edit');
-const popupCloseImageButtonElement = popupEditElement.querySelector('.popup__close_image');
+const popupCloseImageButtonElement = popupImageElement.querySelector('.popup__close_image');
 const popupOpenEditButtonElement = document.querySelector('.profile__edit-button');
 const popupOpenAddButtonElement = document.querySelector('.profile__add-button');
 const formElement = document.querySelector('.popup__content');
@@ -16,13 +16,14 @@ const profileTitleElement = document.querySelector('.profile__title');
 const profileTextElement = document.querySelector('.profile__text');
 const elementsTemplate = document.querySelector('#elements-template').content;
 const elementsElement = document.querySelector('.elements__element');
-const signatureElement = document.querySelector('.elements__signature');
+//const signatureElement = document.querySelector('.elements__signature');
 const sectionElements = document.querySelector('.elements');
 const addButton = popupAddElement.querySelector('.popup__button-save_add');
-const deleteButton = document.querySelector('.elements__delete');
+const deleteButton = document.querySelectorAll('.elements__delete');
 const popupSignature = document.querySelector('.popup__signature');
 const popupFigure = document.querySelector('.popup__figure');
-const maskGroupElement = document.querySelector('.elements__mask-group');
+const maskGroupElement = document.querySelectorAll('.elements__mask-group');
+const titleElement = document.querySelectorAll('.elements__title');
 
 const initialCards = [
   {
@@ -51,21 +52,33 @@ const initialCards = [
   }
 ];
 
-
+// Добавление карточек из массива.
 initialCards.forEach(function (element) {
   const cardElement = elementsTemplate.querySelector('.elements__element').cloneNode(true);
-
+  
   cardElement.querySelector('.elements__title').textContent = element.name;
   cardElement.querySelector('.elements__mask-group').src = element.link;
   cardElement.querySelector('.elements__group').addEventListener('click', function(evt){ 
     evt.target.classList.toggle('elements__group_active');
   });
-
+  cardElement.querySelector('.elements__mask-group').addEventListener('click', function(evt){ 
+    evt.target.classList.add(openPopup(popupImageElement));
+    popupPhoto.src = cardElement.querySelector('.elements__mask-group').src;
+    popupSignature.textContent = cardElement.querySelector('.elements__title').textContent;
+  });
+  
   sectionElements.append(cardElement);
+})
 
-});
+// Кнопка удаления карточкию Не реботает!! 
+/*deleteButton.addEventListener('click', () => {
+  const elementsElement = deleteButton.closest('.elements__element');
+  let index = elementsElement.indexof(elementsElement);
+    elementsElement.splice(index, 1);
+  });*/
 
 
+//Данные профиля
 function handleFormSubmit(evt) {
     evt.preventDefault();
 
@@ -75,20 +88,7 @@ function handleFormSubmit(evt) {
     closePopup(popupEditElement);
 }
 
-/*
-function cardsFormSubmit(evt) {
-  evt.preventDefault();
-
-  initialCards.unshift({
-    name: placeInput.value,
-    link: linkInput.value
-  });
-
-  closePopup(popupAddElement);
-}
-*/
-
-//Попап Опен
+//Попап профиля (общий openPopup)
 function openPopup(popupElement) {
   nameInput.value = profileTitleElement.textContent;
   jobInput.value = profileTextElement.textContent;
@@ -96,24 +96,30 @@ function openPopup(popupElement) {
 }
 
 //Попап Image Опен
-/*
-function maskGroupElement (evt) {
-  evt.preventDefault();
-  popupImageElement.classList.add('popup_opened');
-}
-*/
+/*function imageFormSubmit() {
 
-//закрытие попап
+  cardElement.querySelector('.popup__photo').src = element.link;
+  cardElement.querySelector('.elements__signature').textContent = element.name;
+  
+}*/
+
+
+//Функии закрытия попап
 function closePopup(popupAddElement) {
   popupAddElement.classList.remove('popup_opened');}
 
 function closePopup(popupEditElement) {
   popupEditElement.classList.remove('popup_opened');}
 
+function closePopup(popupImageElement) {
+  popupImageElement.classList.remove('popup_opened');}
+
+//Функии закрытия попап по Overlay
 function closePopupByClickOnOverlay(evt) {
   if (event.target == event.currentTarget) {
-    closePopup(popupElement);
-  }
+    closePopup(popupEditElement);
+    closePopup(popupAddElement);
+    closePopup(popupImageElement);}
 }
 
 
@@ -121,7 +127,9 @@ popupOpenEditButtonElement.addEventListener('click', () => openPopup(popupEditEl
 popupOpenAddButtonElement.addEventListener('click', () => openPopup(popupAddElement));
 popupCloseEditButtonElement.addEventListener('click', () => closePopup(popupEditElement));
 popupCloseAddButtonElement.addEventListener('click',() => closePopup(popupAddElement));
-popupElement.addEventListener('click', () => closePopupByClickOnOverlay);
+popupCloseImageButtonElement.addEventListener('click',() => closePopup(popupImageElement));
+popupEditElement.addEventListener('click', closePopupByClickOnOverlay);
+popupAddElement.addEventListener('click', closePopupByClickOnOverlay);
+popupImageElement.addEventListener('click', closePopupByClickOnOverlay);
 formElement.addEventListener('submit', handleFormSubmit);
-
-//popupCloseImageButtonElement.addEventListener('click',() => closePopup(popupImageElement));
+//maskGroupElement.addEventListener('click', openPopup(popupImageElement));
